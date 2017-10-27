cfcd02091e791df3a9997344be5f4d43f31ceb67
ewu9995 <32362354+ewu9995@users.noreply.github.com>
2017-10-27 09:28:12 -0400

    Changed runner but not finished so expect errors
    Use Levin's runner instead to test.

--- ChatBot/src/ChatBotRunner.java
+++ ChatBot/src/ChatBotRunner.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 /**
  * A simple class to run our chatbot teams.
@@ -10,10 +11,25 @@ public class ChatBotRunner
 
 	/**
 	 * Create instances of each chatbot, give it user input, and print its replies. Switch chatbot responses based on which chatbot the user is speaking too.
+	 * @return 
 	 */
 	public static void main(String[] args)
 	{
-		ChatBotLevin chatbot1 = new ChatBotLevin();
+		System.out.println("DRUG ABUSE CHAT");
+		System.out.println("\n");
+		System.out.println("NOTE: THIS CHATBOT IS IN DEVELOPMENT SO EXPECT SOME BUGS.");
+		
+		public String getAnswer()
+		{
+			return "Hi, what is up?";
+		}
+		
+		ChatBotKim chatbot1 = new ChatBotKim();
+		ChatBotLiang chatbot2 = new ChatBotLiang();
+		ChatBotLau chatbot3 = new ChatBotLau();
+		ChatBotWu chatbot4 = new ChatBotWu();
+		
+	
 		
 		System.out.println (chatbot1.getGreeting());
 		Scanner in = new Scanner (System.in);
