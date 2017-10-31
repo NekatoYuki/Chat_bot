fe07ce7eb987cd0282ab9a209fbf1c7386f27caf
NekatoYuki <32362367+NekatoYuki@users.noreply.github.com>
2017-10-31 09:09:44 -0400

    Update ChatBotKim.java

--- ChatBot/src/ChatBotKim.java
+++ ChatBot/src/ChatBotKim.java
@@ -41,6 +41,10 @@ public class ChatBotKim
 		{
 			response = "I see. May I ask for your name please?";
 		}
+		else if (findKeyword(statement, "call")>=0)
+		{
+			response = "You can call and talk to one of our support members personally at 1-800-555-5555."
+		}
 		else if (findKeyword(statement, "Bye") >= 0)
 		{
 			System.out.println("Enjoy your day and stay safe from drug!!! ^-^");
@@ -49,6 +53,73 @@ public class ChatBotKim
 			String[] args = new String[0] ;
 		    ChatBotRunner.main(args);
 		}
+		else
+		{
+			response = getRandomResponse();
+		}
+		return response;
+	}
+	private String transformIWantToStatement(String statement)
+	{
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement
+					.length() - 1);
+		}
+		int psn = findKeyword (statement, "I want to", 0);
+		String restOfStatement = statement.substring(psn + 9).trim();
+		return "Do you really believe it's ok to" + restOfStatement + "?" + " Doing" + restOfStatement + "will be bad for your overall health";
+	}
+	private int findKeyword(String statement, String goal,
+			int startPos)
+	{
+		String phrase = statement.trim().toLowerCase();
+		goal = goal.toLowerCase();
+
+		// The only change to incorporate the startPos is in
+		// the line below
+		int psn = phrase.indexOf(goal, startPos);
+
+		// Refinement--make sure the goal isn't part of a
+		// word
+		while (psn >= 0)
+		{
+			// Find the string of length 1 before and after
+			// the word
+			String before = " ", after = " ";
+			if (psn > 0)
+			{
+				before = phrase.substring(psn - 1, psn);
+			}
+			if (psn + goal.length() < phrase.length())
+			{
+				after = phrase.substring(
+						psn + goal.length(),
+						psn + goal.length() + 1);
+			}
+
+			// If before and after aren't letters, we've
+			// found the word
+			if (((before.compareTo("a") < 0) || (before
+					.compareTo("z") > 0)) // before is not a
+											// letter
+					&& ((after.compareTo("a") < 0) || (after
+							.compareTo("z") > 0)))
+			{
+				return psn;
+			}
+
+			// The last position didn't work, so let's find
+			// the next, if there is one.
+			psn = phrase.indexOf(goal, psn + 1);
+
+		}
+
+		return -1;
 	}
 	private String getRandomResponse ()
 	{
