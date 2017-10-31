de2fc94f61e77d06f1fc8a797c3d95da6679391d
Kenneth Kim <Kenneth Kim@192.168.1.169>
2017-10-30 22:12:21 -0400

    edit 1

--- ChatBot/src/ChatBotKim.java
+++ ChatBot/src/ChatBotKim.java
@@ -6,6 +6,33 @@ public class ChatBotKim
 	
 	public String getGreeting()
 	{
-		System.out.println
+		System.out.println("\n");
+		System.out.println("Drug Abuse & Addiction Support");
+		System.out.println("To return to the main menu simply enter the word 'bye'");
+		return "You have reached Drug Abuse & Addiction Support."
+		return "Are you reaching support for yourself or someone you know?"
+	}
+	Scanner input = new Scanner(System.in);
+	
+	public String getResponse(String statement)
+	{
+		String response = "";
+		if (statement.length()==0)
+		{
+			response = "Please indicate who is in need of support.";
+		}
+		else if (findKeyword(statement, "no")>=0)
+		{
+			response = "Are you sure you aren't in need of support for someone or yourself?";
+			if (findKeyword(statement, "yes")>=0)
+			{
+				response = "Ok then. Please enter 'bye' to return to the main menu";
+			}
+			else if (findKeyword(statement, "no")>=0)
+			{
+				response = "Please indicate who is in need of support.";
+			}
+		}
+		
 	}
 }
