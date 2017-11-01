ec1991aa8cea100a79541511c9c83e7d06cec71e
NekatoYuki <32362367+NekatoYuki@users.noreply.github.com>
2017-10-31 22:12:18 -0400

    edit 15-ish? (lost count >.>)

--- ChatBot/src/ChatBotKim.java
+++ ChatBot/src/ChatBotKim.java
@@ -132,13 +132,7 @@ public class ChatBotKim
 	{
 		String phrase = statement.trim().toLowerCase();
 		goal = goal.toLowerCase();
-
-		// The only change to incorporate the startPos is in
-		// the line below
 		int psn = phrase.indexOf(goal, startPos);
-
-		// Refinement--make sure the goal isn't part of a
-		// word
 		while (psn >= 0)
 		{
 			// Find the string of length 1 before and after
@@ -150,28 +144,17 @@ public class ChatBotKim
 			}
 			if (psn + goal.length() < phrase.length())
 			{
-				after = phrase.substring(
-						psn + goal.length(),
-						psn + goal.length() + 1);
+				after = phrase.substring(psn + goal.length(),psn + goal.length() + 1);
 			}
 
 			// If before and after aren't letters, we've
 			// found the word
-			if (((before.compareTo("a") < 0) || (before
-					.compareTo("z") > 0)) // before is not a
-											// letter
-					&& ((after.compareTo("a") < 0) || (after
-							.compareTo("z") > 0)))
+			if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0) && ((after.compareTo("a") < 0) || (after.compareTo("z") > 0)))
 			{
 				return psn;
 			}
-
-			// The last position didn't work, so let's find
-			// the next, if there is one.
 			psn = phrase.indexOf(goal, psn + 1);
-
 		}
-
 		return -1;
 	}
 	private int findKeyword(String statement, String goal)
