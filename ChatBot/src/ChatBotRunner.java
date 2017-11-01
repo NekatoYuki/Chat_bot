ab3715961a36818aeb4f486a52cc79e5f0c7f42a
ewu9995 <32362354+ewu9995@users.noreply.github.com>
2017-10-31 21:32:51 -0400

    Update ChatBotRunner.java

--- ChatBot/src/ChatBotRunner.java
+++ ChatBot/src/ChatBotRunner.java
@@ -3,8 +3,9 @@ import java.util.Random;
 
 /**
  * A simple class to run our chatbot teams.
- * @author Mr. Levin
- * @version September 2017
+ * @author Mr. Levin - Starter Code
+ 		Kenneth Kim, Jeffery Lau, Stanley Liang, Evan Wu - Modified Code
+ * @version September 2017; Updated October 2017
  */
 @SuppressWarnings("unused")
 public class ChatBotRunner
@@ -14,16 +15,17 @@ public class ChatBotRunner
 	public static void main(String[] args)
 	{
 		
-
+		//Value storage for chatbot names?
 		ChatBotKim chatbot1 = new ChatBotKim();
 		ChatBotLau chatbot2 = new ChatBotLau();
 		ChatBotLiang chatbot3 = new ChatBotLiang();
 		ChatBotWu chatbot4 = new ChatBotWu();
 		
+		//Menu info text screen
 		System.out.println("\n");
 		System.out.println("Drug awareness chat");
 		System.out.println("\n");
-		System.out.println("Welcome to the drug awareness chat. Please select a topic.");
+		System.out.println("Welcome to the drug awareness chat. Please select a topic/chatbot.");
 		System.out.println("1.Drug abuse/addiction support");
 		System.out.println("2.Drug information");
 		System.out.println("3.Drug awareness");
@@ -32,7 +34,8 @@ public class ChatBotRunner
 		Scanner input = new Scanner(System.in);
 		String choose = input.nextLine();
 	
-
+	//User input must be a number or else the code will return bye and break
+	//When user inputs a choice number the choice selected chatbot will load
 	if (choose.equals("1")) {
 	System.out.println (chatbot1.getGreeting());
 	Scanner in = new Scanner (System.in);
@@ -83,6 +86,7 @@ public class ChatBotRunner
 		}
 	}
 	
+	//Returns bye if user selects a choice that is not a choce number
 	else {
 		System.out.println("Bye!");
 	}
